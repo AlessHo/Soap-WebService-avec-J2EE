5a3cfdcb4540d6e031f10bbaa04f9c09ae4d8220
AlessHo <alesshosry@hotmail.com>
2017-03-20 20:27:10 +0200

    Client VB .Net

---
+++ Client VB .Net/WebserviceClient/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports WebserviceClient.localhost
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim s As New MyServiceClassService
+        Dim number1 = TextBox1.Text
+        Dim number2 = TextBox2.Text
+        Dim result = s.AddNumbers(number1, number2)
+        Label3.Text = result
+    End Sub
+
+End Class
